caa873b433bcc477983793b798cac62e6eaef302
Marsuazo23 <mahilysuazo2004@gmail.com>
2024-11-23 17:50:02 -0600

    Practica de sliders/java

--- js/script.js
+++ js/script.js
@@ -1,128 +1,120 @@
-document.addEventListener("DOMContentLoaded", function () {
-    //alert("Content Loaded desde Script externo js");
-
+document.addEventListener("DOMContentLoaded", function (){
     let bolEdad = true;
-   console.log("Iniciando JS", bolEdad); 
-   let resultado;
-   resultado = 10 + 30;
-   resultado2 = parseFloat("10.999") + 30;
-   console.log("Comparar Operaciones:", {resultado, resultado2});
-   
-   resultado = 10 - 5;
-   console.log("Resta", resultado);
+    console.log("Iniciando JS", bolEdad);
+    let resultado, resultado2;
+    resultado = 10 + 30;
+    resultado2 = parseFloat("10.9999") + 30;
+    console.log("Comparar Operaciones:", {resultado, resultado2});
 
-   resultado = 10 * 5;
-   console.log("Multiplicación", resultado);
+    resultado = 10 - 5;
+    console.log("Resta", resultado);
 
-   resultado = 10 / 5;
-   console.log("División", resultado);
+    resultado = 10 * 5;
+    console.log("Multi", resultado);
 
-   resultado = 10 % 3;
-   console.log("Módulo", resultado);
+    resultado = 10 / 5;
+    console.log("Div", resultado);
 
-   resultado++;
-   resultado--;
+    resultado = 10 % 3;
+    console.log("Módulo", resultado);
 
-   resultado += 10;
-   resultado -= 5;
-   resultado *= 2;
-   resultado /= 3;
+    resultado++;
+    resultado--;
 
-   let strMensaje = `El resultado: ${resultado}`;
-   let strMensaje2 = 'El resultado: ' + resultado; 
+    resultado += 10;
+    resultado -= 5;
+    resultado *= 2;
+    resultado /= 3;
 
-   let strDoubleQuotes = "Esto es \"UNA CITA Bibliografica\" se debe usar \\ para escapar el simbolo ";
-   let strSingleQuotes = 'Esto es "UNA CITA Bibliografica" no se debe usar \ para escapar el simbolo';
+    let strMensaje = `El resultado: ${resultado}`;
+    let strMensaje2 = 'El resultado: ' + resultado;
 
-   const objPersona = {
-    "nombre" : "Marcela",
-    "apellido" : "Suazo",
-    "edad": 19,
-    "correo" : "marcesuazo@gmail.com"
-   }; //JSON OBJETC NOTATION JSON
+    let strDoubleQuotes = "Esto es \"UNA CITA Bibliografica\" se debe usar \\ para escapar el simbolo";
+    let strSingleQuotes = 'Esto es "UNA CITA Bibliográfica" no se debe usar \ para escapar el simbolo';
 
-   objPersona.edad = 20;
+    const objPersona = {
+        "nombre" : "Orlando",
+        "apellido" : "Betancourth",
+        "edad": 40,
+        "correo": "obetancourthunicah@gmail.com"
+    }; // JSON OBJECT NOTATION  JSON
 
-   const demoEdad = 10;
-   //demoEdad = 20;
+    objPersona.edad = 20;
 
-   
-   const arrPersonas = [];
+    //const demoEdad = 10;
+    //demoEdad = 20;
 
-   arrPersonas.push(objPersona);
+    const arrPersonas = [];
 
-   //arrPersonas = []; 
+    arrPersonas.push(objPersona);
 
-   //Estructuras de control -- IF, Switch, Try catch
-   let a = 10;
-   let b = "10";
-   if (a == b) {
-    console.log("Si entro a A == B");
-   }
-   if (a === b) {
-    console.log("Si entro a A == B");
-   }
-   else{
-    console.log("No entro a A == B");
-   }
+    //arrPersonas = [];
 
-   //Negaciones
-   if (a != b) {
-    console.log("Si entro a A != B");
-   }
-   else{
-    console.log("No entro a A != B");
-   }
+    let a = 10;
+    let b = "10";
+    if (a == b) {
+        console.log("Si entro a A == B");
+    }
+    if (a === b) {
+        console.log("Si entro a A === B");
+    } else {
+        console.log("No entro a A == B");
+    }
+    if (a != b) {
+        console.log("Si entro a A != B");
+    }else {
+        console.log("No entro a A != B");
+    }
+    if (a !== b) {
+        console.log("Si entro a A !== B");
+    } else {
+        console.log("No entro a A !== B");
+    }
 
-   if (a !== b) {
-    console.log("Si entro a A !== B");
-   }
-   else{
-    console.log("No entro a A !== B");
-   }
+    const strGreaterThan10 = (a > 10) ? "A es mayor a 10":"A es menor o igual a 10";
+    console.log("Ternaria:" , strGreaterThan10);
 
-   const strGreaterThan10 = ( a > 10) ? "A es mayor a 10": "A es menor o igual a 10";
-   console.log("Ternaria:", strGreaterThan10)
+    let strValueSelected = 'ACT'; // ACT| INA | SPN | RTR | CNT
+    switch(strValueSelected) {
+        case "ACT":
+            console.log("SelectedValue:", strValueSelected);
+            break;
+        case "INA":
+            // Epresiones
+            break;
+        case "SPN":
+            // Epresiones
+            break;
+        default:
+            console.log("ERROR", "No hay handlera para expresion");
+            // Expresiones
+            break;
+    }
 
-   let strValueSelected = 'ACT'; //ACT - INA - SPN - RTR - CNT
-   switch(strValueSelected){
-    case "ACT":
-        console.log("Selecte");
-    break;
-    case "INA":
-        //Expresiones
-    break;
-    case "SPN":
-        //Expresiones
-    break;
-    default:
-        console.log("ERROR", "No hay handlera para expresion");
-        break;
-   }
+    // Estructuras de Ciclos o Iteraciones.
+    for (let i=0; i<10; i++){
+        console.log("Iteracion for: ", i);
+    }
+    let intWhile = 10;
+    while ( intWhile >= 0){
+        console.log("Iteracion while: ", intWhile);
+        intWhile -= 1;
+    }
 
-   //Estructuras de ciclos o iteraciones
-   for (let i=0; i < 10; i++){
-    console.log("Iteracion for; ", i);
-   }
+    let intDoWhile = 50;
+    do {
+        console.log("Iteración Do While", intDoWhile);
+        intDoWhile--;
+    } while (intDoWhile < 10);
 
-   let intWhile = 10;
-   while(intWhile >= 0){
-    console.log("Iteracion while: ", intWhile);
-    intWhile --;
-   }
-   let intDoWhile = 10;
-   do {
-    console.log("Iteraccion DO While: ", intDoWhile);
-    intDoWhile++;
-   }while (intDoWhile > 10)
-
-    const arrColores = ("red", "blue", "green", 0x1234ab, 255);
+    const arrColores = ["red","blue","green", 123, 255];
 
     arrColores.forEach(
-        (color) => {
+        (color)=>{
             console.log("Color Iterando: ", color);
         }
     );
 
     const miSlider = new SliderController();
+
 });
